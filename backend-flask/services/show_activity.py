01d0be079279dd5110f2bfcc737b1ab769c48be6
Oshim Thakur <oshimthakur@gmail.com>
2025-09-03 12:48:54 +0000

    minor changes to show activity

--- backend-flask/services/show_activity.py
+++ backend-flask/services/show_activity.py
@@ -10,11 +10,11 @@ class ShowActivity:
             'message': 'Cloud is fun!',
             'created_at': (now - timedelta(days=2)).isoformat(),
             'expires_at': (now + timedelta(days=5)).isoformat(),
-            'replies': {
+            'replies': [{
                 'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
                 'handle': 'Worf',
                 'message': 'This post has no honor!',
                 'created_at': (now - timedelta(days=2)).isoformat()
-            }
+            }]
         }]
         return results
