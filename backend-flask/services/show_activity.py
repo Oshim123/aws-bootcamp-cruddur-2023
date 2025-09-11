de02662712b55bd72592af35b88f1a314e7979dd
Oshim Thakur <oshimthakur@gmail.com>
2025-09-11 12:24:24 +0000

    updated to my own name

--- backend-flask/services/show_activity.py
+++ backend-flask/services/show_activity.py
@@ -6,8 +6,8 @@ class ShowActivity:
         now = datetime.now(timezone.utc).astimezone()
         results = [{
             'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-            'handle': 'Andrew Brown',
-            'message': 'Cloud is fun!',
+            'handle': 'Oshim Thakur',
+            'message': 'Cloud is something else!',
             'created_at': (now - timedelta(days=2)).isoformat(),
             'expires_at': (now + timedelta(days=5)).isoformat(),
             'replies': [{
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -16,8 +16,8 @@ class UserActivities:
         now = datetime.now()
         results = [{
           'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Andrew Brown',
-          'message': 'Cloud is fun!',
+          'handle':  'Oshim Thakur',
+          'message': 'Cloud is something else!',
           'created_at': (now - timedelta(days=1)).isoformat(),
           'expires_at': (now + timedelta(days=31)).isoformat()
         }]
