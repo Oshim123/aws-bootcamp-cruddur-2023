2966675e3084ff17fc9264024af6762a723ff046
Oshim Thakur <oshimthakur@gmail.com>
2025-09-16 18:44:28 +0000

    updated for new version of amplify

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -3,7 +3,8 @@ import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import React from "react";
 
 // [TODO] Authenication
-import { Auth } from 'aws-amplify';
+import { Auth } from '@aws-amplify/auth';
+
 
 export default function ProfileInfo(props) {
   const [popped, setPopped] = React.useState(false);
